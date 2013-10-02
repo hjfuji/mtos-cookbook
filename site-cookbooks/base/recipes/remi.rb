02bc8601b0d1050dbb67d5c6928e17db65dd0bbf
Hajime Fujimoto <hajime2008x@yahoo.co.jp>
2013-10-03 08:51:36 +0900

    Forgot adding remi.rb

---
+++ site-cookbooks/base/recipes/remi.rb
@@ -0,0 +1,17 @@
+include_recipe "yum::epel"
+
+yum_key node['yum']['remi']['key'] do
+  url  node['yum']['remi']['key_url']
+  action :add
+end
+
+yum_repository "remi" do
+  description "Les RPM de remi pour Enterprise Linux #{node['platform_version']} - $basearch"
+  key node['yum']['remi']['key']
+  mirrorlist node['yum']['remi']['url']
+  failovermethod "priority"
+  includepkgs node['yum']['remi']['includepkgs']
+  exclude node['yum']['remi']['exclude']
+  enabled 0
+  action :create
+end
